9091d299f219ff7e8043d6744218197d31df94cb
Ram89100 <ram89100@gmail.com>
2026-01-10 20:01:07 +0530

    Added Django app - jobs

---
+++ jobs/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+from .models import Job
+from .serializers import JobSerializer
+
+class JobListView(generics.ListAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+class JobDetailView(generics.RetrieveAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    lookup_field = "task_id"
+from django.shortcuts import render
+
+# Create your views here.
